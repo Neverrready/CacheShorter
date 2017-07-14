0a52f2e45653ed6806c50d1aa93e6ea78d681a6c
Clayton Flesher <claytonflesher@gmail.com>
2017-07-14 14:04:41 -0500

    updated configuration to new location

--- scripts/start_nginx.sh
+++ scripts/start_nginx.sh
@@ -2,6 +2,7 @@
 
 set -e -x
 
+cp sites/default.conf /etc/nginx/conf.d/default.conf
 cp nginx.conf /etc/nginx/nginx.conf
 cp sites/www/html/index.html /var/www/html/index.html
 nginx -g "daemon off;"
